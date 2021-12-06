f33388a5c8debfa93d6739b54be739d56ea8f705
Jack Sanders <jsanders4@sheffield.ac.uk>
2021-12-06 13:23:17 +0000

    Did a lot - comments and tabindex mainly

--- js/showNavButton.js
+++ js/showNavButton.js
@@ -1,5 +1,6 @@
 function changeNavVisibleMobile() {
     nav.classList.toggle('nav-shown'); // Expands nav menu
+    toggleNavFocus() // Toggle whether nav items can be selected by tab
 
     // Changes button text
     if (showButtonMobile.innerHTML === "Menu") {
@@ -11,6 +12,7 @@ function changeNavVisibleMobile() {
 
 function changeNavVisibleDesktop() {
     nav.classList.toggle('nav-shown'); // Expands nav menu
+    toggleNavFocus() // Toggle whether nav items can be selected by tab
     showButtonDesktop.style.opacity = '0';
     if (showButtonDesktop.innerHTML === '<img src="img/menuClosed.png"><p>Menu</p>') {
         setTimeout(function (){
@@ -25,9 +27,25 @@ function changeNavVisibleDesktop() {
     }
 }
 
+function toggleNavFocus() {
+    /* Select list of children (li elements) of third child (ul element) of
+     nav */
+    let listItems = nav.children[2].children;
+    for (let i=0; i<listItems.length; i++) {
+        /* Select only child (anchor element) of li element */
+        let anchor = listItems[i].children[0];
+        if (anchor.tabIndex === 0) {
+            anchor.tabIndex = -1;
+        } else {
+            anchor.tabIndex = 0;
+        }
+    }
+}
+
 let showButtonMobile = document.getElementById('show-nav-mobile');
 let showButtonDesktop = document.getElementById('show-nav-desktop');
 let nav = document.getElementsByTagName("nav")[0];
 showButtonMobile.addEventListener('click', changeNavVisibleMobile, false);
 showButtonDesktop.addEventListener('click', changeNavVisibleDesktop, false);
 showButtonDesktop.innerHTML = '<img src="img/menuClosed.png"><p>Menu</p>'; // Fixes innerHTML to match if statement
+toggleNavFocus();
