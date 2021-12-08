ee9e57b7d5f3bc6c17ac4835f89863739d7f3964
Jack Sanders <jsanders4@sheffield.ac.uk>
2021-12-08 22:36:56 +0000

    Finished game

--- js/canvasSize.js
+++ js/canvasSize.js
@@ -2,37 +2,74 @@ function resetCanvasSize() {
     /* Function to automatically resize the game canvas when the page is
      loaded/reloaded/resized, to take up the maximum space possible
      */
-    var canvas = document.getElementById('game-canvas');
-    var windowHeight = window.innerHeight;
-    var windowWidth = window.innerWidth;
+    let loadedItems = GameGlobals.drawObjects;
+    let canvas = document.getElementById('game-canvas');
+    let windowHeight = window.innerHeight;
+    let windowWidth = window.innerWidth;
     // https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
-    var notCanvas = document.querySelectorAll('body>*:not(main, nav),' +
+    let notCanvas = document.querySelectorAll('body>*:not(main, nav),' +
         ' main>*:not(#game-canvas), nav button');
-    var elementsHeight = 0;
+    let elementsHeight = 0;
+
+    let originalWidth = canvas.width;
+    let originalHeight = canvas.height;
 
     // Get total height of all elements that aren't the canvas.
     for (let i=0; i<notCanvas.length; i++) {
        elementsHeight += notCanvas[i].offsetHeight;
     }
 
-    if ((windowHeight - elementsHeight) * 0.9 < 450) {
+    if ((windowHeight - elementsHeight) * 0.9 < 500) {
         /* Minimum height, any smaller than this and the game would be
          uncomfortable to play
          */
-        canvas.height = 450;
+        canvas.height = 500;
     } else {
         // Set height to 90% of available space
         canvas.height = (windowHeight - elementsHeight) * 0.9;
     }
     // Take 90% of screen width to match other elements
     canvas.width = windowWidth * 0.9;
+
+    if (!loadedItems.includes(undefined)) {
+        let xScale = canvas.width / originalWidth;
+        for (let i=0; i<loadedItems.length; i++) {
+            if (canvas.height !== originalHeight) {
+                let imageHeight = (canvas.height - 80) / 3
+                loadedItems[i].setHeight(imageHeight)
+                var y = 65 + imageHeight*i;
+
+                console.log(imageHeight, y);
+            }
+            loadedItems[i].setCoords(loadedItems[i].x * xScale, y);
+            loadedItems[i].draw();
+        }
+    }
+
+    let newSize;
+    if (window.innerWidth > 550 && originalWindowWidth <= 550) {
+        newSize = GameGlobals.font.substring(0, 2) * 2;
+    } else if (window.innerWidth <= 550 && originalWindowWidth > 550) {
+        newSize = GameGlobals.font.substring(0, 2) / 2;
+    } else {
+        newSize = GameGlobals.font.substring(0, 2);
+    }
+
+    console.log(newSize);
+    GameGlobals.setFont(newSize + "px Verdana");
+
+    let textX = canvas.width / 2;
+    GameGlobals.ctx.textAlign = 'center'
+    GameGlobals.ctx.fillText(GameGlobals.currentText, textX, 40);
+
+    originalWindowWidth = window.innerWidth;
 }
 
 function resizeAction() {
     /* This prevents all of the article size calculations being performed
      thousands of times every time the window is reset.
      */
-    if (timer === 'false') {
+    if (timer === false) {
         /* If timer is not running, start it */
         timer = window.setTimeout(resetCanvasSize, 500);
     } else {
@@ -42,6 +79,8 @@ function resizeAction() {
     }
 }
 
-let timer = 'false';
+let timer = false;
+let originalWindowWidth = window.innerWidth;
+
 window.addEventListener('load', resetCanvasSize);
 window.addEventListener('resize', resizeAction);
--- js/playGame.js
+++ js/playGame.js
@@ -1,45 +1,333 @@
+function Globals() {
+    this.playButton = document.getElementById('play-game')
+    this.nameInput = document.getElementById('game-name');
+    this.nameButton = document.getElementById('submit-name');
+    this.nextRound = document.getElementById('next-round');
+    this.canvas = document.getElementById('game-canvas');
+    this.font = "20px Verdana";
+    this.ctx = document.getElementById('game-canvas').getContext('2d');
+    this.currentText = "";
+    this.roundCount = 0;
+    this.rightAnswers = 0;
+
+    this.drawObjects = new Array(3);
+}
+
+Globals.prototype.setFont = function (font) {
+    this.font = font;
+    this.ctx.font = font;
+}
+
+Globals.prototype.reset = function () {
+    this.font = "20px Verdana";
+    this.currentText = "";
+    this.roundCount = 0;
+    this.rightAnswers = 0;
+
+    this.drawObjects = new Array(3);
+}
+
+function Circle(x, y, size) {
+    this.x = x;
+    this.centreX = x + size / 2;
+    this.y = y;
+    this.centreY = y + size / 2;
+    this.size = size;
+    this.radius = size * 0.45;
+}
+
+Circle.prototype.setCoords = function(x, y=null) {
+    /* Use one function for both coordinates, rather than needing one for x
+     and one for y.
+     */
+    if (x !== null) {
+        /* As radius is 45% of the 'bounding box', divide by 9 to get 5%,
+         and multiply by 10 to get 50%, to put the coordinates in the centre
+          of the box.
+         */
+        this.x = x;
+        this.centreX = x + this.size / 2;
+    }
+    if (y !== null) {
+        this.y = y;
+        this.centreY = y + this.size / 2;
+    }
+}
+
+Circle.prototype.setHeight = function (height) {
+    this.size = height;
+    this.radius = height * 0.45;
+}
+
+Circle.prototype.draw = function () {
+    let ctx = GameGlobals.ctx;
+    ctx.beginPath();
+    ctx.arc(this.centreX, this.centreY, this.radius, 0, Math.PI*2);
+    ctx.stroke();
+}
+
+Circle.prototype.clear = function () {
+    GameGlobals.ctx.clearRect(this.x, this.y, this.size, this.size);
+}
+
+Circle.prototype.getType = function () {
+    return 'circle';
+}
+
+Circle.prototype.checkMousePos = function (mX, mY) {
+    if (Math.sqrt((this.centreX - mX)**2+(this.centreY - mY)**2) <= this.radius) {
+        return true;
+    }
+    return false;
+}
+
+function Triangle(x, y, height) {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+}
+
+Triangle.prototype.setCoords = function(x, y=null) {
+    /* Use one function for both coordinates, rather than needing one for x
+     and one for y.
+     */
+    if (x !== null) {
+        this.x = x;
+    }
+    if (y !== null) {
+        this.y = y;
+    }
+}
+
+Triangle.prototype.setHeight = function (height) {
+    this.height = height;
+}
+
+Triangle.prototype.draw = function () {
+    let ctx = GameGlobals.ctx
+    let triangleX = this.x + this.height * 0.05;
+    let triangleY = this.height + (this.y - this.height * 0.05);
+    let triangleHeight = this.height * 0.9;
+    ctx.beginPath();
+    ctx.moveTo(triangleX, triangleY);
+    ctx.lineTo(triangleX + triangleHeight, triangleY);
+    ctx.lineTo(triangleX + triangleHeight / 2, triangleY - triangleHeight)
+    ctx.lineTo(triangleX, triangleY);
+    ctx.stroke();
+}
+
+Triangle.prototype.clear = function () {
+    GameGlobals.ctx.clearRect(this.x, this.y, this.height, this.height);
+}
+
+Triangle.prototype.getType = function () {
+    return 'triangle';
+}
+
+Triangle.prototype.checkMousePos = function (mX, mY) {
+    if (mX > this.x && mX < this.x + this.height) {
+        if (mY > this.y && mY < this.y + this.height) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Define object for images, so they can easily be manipulated
+function GameImage(img, x, y, sideLength) {
+    this.img = img;
+    this.x = x;
+    this.y = y + 5;
+    this.sideLength = sideLength - 10;
+}
+
+GameImage.prototype.draw = function () {
+    GameGlobals.ctx.drawImage(this.img, this.x, this.y, this.sideLength, this.sideLength);
+}
+
+GameImage.prototype.clear = function () {
+    GameGlobals.ctx.clearRect(this.x, this.y, this.sideLength, this.sideLength);
+}
+
+GameImage.prototype.getType = function () {
+    // Split at / (turn path/to/img.jpg into ['path', 'to', 'img.jpg']
+    let imgSrc = this.img.src.split('/');
+
+    // Select last element (filename) from split filepath and remove extension.
+    imgSrc = imgSrc[imgSrc.length - 1].split('.')[0];
+
+    // Remove last character (the number) from the string
+    imgSrc = imgSrc.substring(0, imgSrc.length - 1);
+    return imgSrc;
+}
+
+GameImage.prototype.setCoords = function(x, y=null) {
+    /* Use one function for both coordinates, rather than needing one for x
+     and one for y.
+     */
+    if (x !== null) {
+        this.x = x;
+    }
+    if (y !== null) {
+        this.y = y;
+    }
+}
+
+GameImage.prototype.setHeight = function (height) {
+    this.sideLength = height;
+}
+
+GameImage.prototype.checkMousePos = function (mX, mY) {
+    if (mX > this.x && mX < this.x + this.sideLength) {
+        if (mY > this.y && mY < this.y + this.sideLength) {
+            return true;
+        }
+    }
+    return false;
+}
+
 function randomNumber(lower, upper) {
     return lower + Math.floor(Math.random() * (upper - lower))
 }
 
-function playGame(images) {
-    let imageNames = [['dog1', 'dog2'], ['cat1', 'cat2'], ['duck1', 'duck2'], ['penguin1', 'penguin2']];
-    let imageHeight = (canvas.height - 30) / 3;
+function finishGame() {
+    GameGlobals.ctx.clearRect(0, 0, GameGlobals.canvas.width, GameGlobals.canvas.height);
+    GameGlobals.currentText = "Nice one "+GameGlobals.name+"! You got "+GameGlobals.rightAnswers+"/3!";
+    GameGlobals.ctx.fillText(GameGlobals.currentText, GameGlobals.canvas.width / 2, 40);
+    GameGlobals.playButton.innerText = "Press here to play again!";
+    GameGlobals.playButton.style.display = 'block';
+}
 
-    let xUpper = canvas.width - 15 - imageHeight;
+function checkImages(event) {
+    let clickX = event.layerX;
+    let clickY = event.layerY;
+    var correct;
 
-    for (let i=0; i<3; i++) {
-        let randType = randomNumber(0, imageNames.length)
-        let chosenType = imageNames[randType];
-        let randImg = randomNumber(0, chosenType.length)
+    if (GameGlobals.drawObjects.includes(undefined)) {
+        return;
+    }
 
-        let toDraw = chosenType[randImg];
-        chosenType.splice(randImg, 1);
-
-        for (let j=0; j<images.length; j++) {
-            let selected = images[j];
-            if (selected.src.endsWith(toDraw + '.jpg')) {
-                var chosenImage = selected;
-                console.log(chosenImage);
-                break;
+    for (let i=0; i<GameGlobals.drawObjects.length; i++) {
+        if (GameGlobals.drawObjects[i].checkMousePos(clickX, clickY)) {
+            if (GameGlobals.drawObjects[i].getType() === GameGlobals.type) {
+                correct = true;
+            } else {
+                correct = false;
+                var incorrectType = GameGlobals.drawObjects[i].getType();
             }
-        }
+            GameGlobals.ctx.clearRect(0, 0, GameGlobals.canvas.width, GameGlobals.canvas.height);
 
-        context.drawImage(chosenImage, randomNumber(15, xUpper), 15 + imageHeight*i, imageHeight, imageHeight);
+            GameGlobals.drawObjects = new Array(3);
+
+            if (correct) {
+                    GameGlobals.currentText = "Well done " + GameGlobals.name + "!\nThat is" +
+                        " a " + GameGlobals.type + "!";
+                    GameGlobals.rightAnswers++;
+            } else {
+                GameGlobals.currentText = "Unlucky " + GameGlobals.name + "!\nThat is a " + incorrectType + ".";
+            }
+
+            if (window.innerWidth > 550) {
+                // Desktop font size
+                GameGlobals.setFont("20px Verdana");
+            } else {
+                GameGlobals.setFont("10px Verdana");
+            }
+            GameGlobals.ctx.fillText(GameGlobals.currentText, GameGlobals.canvas.width / 2, 40);
+
+            if (GameGlobals.roundCount < 3) {
+                GameGlobals.nextRound.style.display = 'block';
+                GameGlobals.nextRound.addEventListener('click', initialiseImageLoad);
+            } else {
+                setTimeout(finishGame, 5000);
+            }
+
+            break;
+        }
     }
 }
 
+function playGame() {
+    // Clear canvas from previous rounds
+    GameGlobals.ctx.clearRect(0, 0, GameGlobals.canvas.width, GameGlobals.canvas.height);
+
+    // Hide next round button
+    GameGlobals.nextRound.style.display = 'none';
+
+    // Increment round counter
+    GameGlobals.roundCount++;
+
+    let imageNames = [['dog1', 'dog2'], ['cat1', 'cat2'], ['duck1', 'duck2'],
+        ['penguin1', 'penguin2'], [new Triangle()], [new Circle()]];
+    let typesChosen = new Array(3);
+    let imageHeight = (GameGlobals.canvas.height - 80) / 3;
+
+    let xUpper = GameGlobals.canvas.width - 15 - imageHeight;
+
+    for (let i=0; i<3; i++) {
+        let randType = randomNumber(0, imageNames.length)
+        while (typesChosen.includes(randType)) {
+            // Make sure we get a unique image type
+            randType = randomNumber(0, imageNames.length);
+        }
+
+        let chosenType = imageNames[randType];
+
+        typesChosen[i] = randType;
+
+        let randImg = randomNumber(0, chosenType.length)
+
+        let toDraw = chosenType[randImg];
+
+        /* If toDraw is a string, that means it is referring to an image,
+         and so we treat it as such. If not, then just add the object
+          directly to the array.
+         */
+        if (typeof toDraw === 'string') {
+            for (let j = 0; j < GameGlobals.images.length; j++) {
+                let selected = GameGlobals.images[j];
+                if (selected.src.endsWith(toDraw + '.jpg')) {
+                    var chosenImage = selected;
+                    GameGlobals.drawObjects[i] = new GameImage(chosenImage, randomNumber(15, xUpper), 65 + imageHeight*i, imageHeight)
+                    break;
+                }
+            }
+        } else {
+            toDraw.setHeight(imageHeight);
+            toDraw.setCoords(randomNumber(15, xUpper), 65 + imageHeight*i);
+            GameGlobals.drawObjects[i] = toDraw;
+        }
+        GameGlobals.drawObjects[i].draw();
+    }
+
+    GameGlobals.type = GameGlobals.drawObjects[randomNumber(0, 3)].getType();
+
+    if (window.innerWidth > 550) {
+        // Desktop font size
+        GameGlobals.setFont("40px Verdana");
+    } else {
+        GameGlobals.setFont("20px Verdana");
+    }
+
+    GameGlobals.ctx.textAlign = "center";
+
+    let textX = GameGlobals.canvas.width / 2;
+    GameGlobals.currentText = 'Click on the '+GameGlobals.type;
+    GameGlobals.ctx.fillText('Click on the '+GameGlobals.type, textX, 40);
+    GameGlobals.canvas.addEventListener('click', checkImages);
+}
+
 function loadImages(files, callback) {
-    var images = new Array(files.length);
+    GameGlobals.images = new Array(files.length);
     var loadedCount = 0;
     for (var i=0; i<files.length; ++i) {
-        images[i] = new Image();
-        images[i].onload = function() {
+        GameGlobals.images[i] = new Image();
+        GameGlobals.images[i].onload = function() {
             loadedCount++;
             if (loadedCount==files.length)
-                callback(images);
+                callback();
         }
-        images[i].src = files[i];
+        GameGlobals.images[i].src = files[i];
     }
 }
 
@@ -50,23 +338,24 @@ function initialiseImageLoad() {
 }
 
 function inputName() {
+    GameGlobals.ctx.clearRect(0, 0, GameGlobals.canvas.width, GameGlobals.canvas.height);
     // Hide play button, show name input
-    playButton.style.display = 'none';
-    nameInput.style.display = 'block';
+    GameGlobals.playButton.style.display = 'none';
+    GameGlobals.nameInput.style.display = 'block';
 }
 
 function getName() {
-    // Get value of second child (text field) of nameInput div
-    let name = nameInput.children[1].value;
-    nameInput.style.removeProperty('display') // back to none - in CSS
+    GameGlobals.reset();
+    // Get value of second child (text field) of GameGlobals.nameInput div
+    GameGlobals.name = GameGlobals.nameInput.children[1].value;
+    console.log(GameGlobals.name);
+    GameGlobals.nameInput.style.removeProperty('display') // back to none - in CSS
 
-    initialiseImageLoad(name);
+    initialiseImageLoad();
 }
 
-const playButton = document.getElementById('play-game');
-const nameInput = document.getElementById('game-name');
-const nameButton = document.getElementById('submit-name');
-const canvas = document.getElementById('game-canvas')
-const context = canvas.getContext('2d');
-playButton.addEventListener('click', inputName);
-nameButton.addEventListener('click', getName);
+// Use this object literal to minimise the number of global variables.
+let GameGlobals = new Globals();
+
+GameGlobals.playButton.addEventListener('click', inputName);
+GameGlobals.nameButton.addEventListener('click', getName);
